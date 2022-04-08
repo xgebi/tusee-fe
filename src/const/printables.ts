3c9deb7d5a07748feb0625c7dcddf2f3d3047f9b
Sarah Gebauer <xgebi@users.noreply.github.com>
2022-04-08 20:37:22 +0200

    Printables prototype is done 🙂

--- src/const/printables.ts
+++ src/const/printables.ts
@@ -1,9 +1,30 @@
+/**
+ * Figure out if this should be here or should be loaded from database.
+ * This is sort of configuration which would need a lot of governments
+ * to change.
+ */
 const PAGE_FORMATS = Object.freeze({
   A5: 'a5',
   A4: 'a4',
 });
 
+const PAGE_SVG_VIEWBOX_DIMENSIONS = Object.freeze({
+  a5: {
+    portrait: { width: 14.8, height: 21, unit: 'cm' },
+    landscape: { width: 21, height: 14.8, unit: 'cm' },
+  },
+  a4: {
+    portrait: { width: 21, height: 29.7, unit: 'cm' },
+    landscape: { width: 29.7, height: 21, unit: 'cm' },
+  },
+});
+
 const ORIENTATION_LANDSCAPE = 'landscape';
 const ORIENTATION_PORTRAIT = 'portrait';
 
-export { PAGE_FORMATS, ORIENTATION_LANDSCAPE, ORIENTATION_PORTRAIT };
+export {
+  PAGE_FORMATS,
+  PAGE_SVG_VIEWBOX_DIMENSIONS,
+  ORIENTATION_LANDSCAPE,
+  ORIENTATION_PORTRAIT,
+};
--- src/views/PrintableView.vue
+++ src/views/PrintableView.vue
@@ -35,28 +35,107 @@
         />
         <label for="landscape">Landscape</label>
       </div>
+      <button @click="printPage">Print page</button>
     </section>
-    <svg :class="svgClass"></svg>
+    <svg
+      :class="svgClass"
+      ref="svgRef"
+      :width="svgWidth"
+      :height="svgHeight"
+      :viewBox="svgViewBox"
+    >
+      <rect y="1cm" x="1cm" width="19cm" height="12.8cm" fill="red"></rect>
+    </svg>
   </main>
 </template>
 
 <script setup lang="ts">
+import { jsPDF } from 'jspdf';
+import 'svg2pdf.js';
+
 import MainNavigation from '@/components/shared/MainNavigation.vue';
 import {
   ORIENTATION_PORTRAIT,
   PAGE_FORMATS,
   ORIENTATION_LANDSCAPE,
+  PAGE_SVG_VIEWBOX_DIMENSIONS,
 } from '@/const/printables';
-import { reactive, computed } from 'vue';
+import { reactive, computed, ref, type Ref } from 'vue';
 
+type pageConfigurationType = {
+  format: string | number[];
+  orientation: 'p' | 'portrait' | 'l' | 'landscape';
+};
 const pageConfiguration = reactive({
   format: PAGE_FORMATS.A5,
   orientation: ORIENTATION_PORTRAIT,
-});
+} as pageConfigurationType);
 
 const svgClass = computed(() => {
   return `${pageConfiguration.format} ${pageConfiguration.orientation}`;
 });
+
+const svgWidth = computed(() => {
+  return `${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].width
+  }${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].unit
+  }`;
+});
+
+const svgHeight = computed(() => {
+  return `${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].height
+  }${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].unit
+  }`;
+});
+
+const svgViewBox = computed(() => {
+  return `0 0 ${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].width
+  }${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].unit
+  } ${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].height
+  }${
+    PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].unit
+  }`;
+});
+
+const svgRef: Ref<HTMLElement | null> = ref(null);
+
+const printPage = () => {
+  const doc = new jsPDF({
+    orientation: pageConfiguration.orientation,
+    format: pageConfiguration.format,
+    unit: PAGE_SVG_VIEWBOX_DIMENSIONS[pageConfiguration.format][
+      pageConfiguration.orientation
+    ].unit,
+  });
+
+  const refSvgValue = svgRef.value as HTMLElement;
+  doc.svg(refSvgValue, {}).then(() => {
+    // save the created pdf
+    doc.output('dataurlnewwindow', { filename: 'tusee-daily.pdf' });
+  });
+};
 </script>
 
 <style lang="scss">
